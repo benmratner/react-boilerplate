11d5f1cc35f6e616976ae3bdada89cef7b84ec1a
Ben Ratner <bratner@temp-marvel.com>
2016-12-07 12:22:50 -0500

    initial commit

---
+++ www/app/config/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Router, Route, IndexRoute, browserHistory, hashHistory} from 'react-router';
+
+import {MainContainer, HomeContainer} from '../containers/';
+
+const routes = (
+	<Router history={browserHistory}>
+		<Router path='/' component={MainContainer}>
+			<IndexRoute component={HomeContainer} />
+		</Router>
+	</Router>
+	);
+
+export default routes;
